11e7492418308392a8849c6cc3f2ecf20eee3c27
deadbread0 <kirakaramisheva2007@gmail.com>
2025-08-21 18:18:31 +0300

    version 3.0

--- quadratic.c
+++ quadratic.c
@@ -5,24 +5,14 @@ int input(float*, float*, float*);
 float squareroot(float);
 void findsolution(float, float, float, int*);
 void output(float, float, float, int);
+char getanswer();
+void BodyOfProgramm();
+
+enum ad {a_and_d_equal_to_zero, only_a_equal_to_zero, only_d_equal_to_zero, a_and_d_equal_to_notzero};
 
 int main()
 {
-    float a = 0;
-    float b = 0;
-    float c = 0;
-    int count_inputnum = 0;
-    int way_of_solution = 0; 
-
-    count_inputnum = input(&a, &b, &c);
-
-    while (count_inputnum==3)
-    {
-        findsolution(a, b, c, &way_of_solution);
-        output(a, b, c, way_of_solution);
-        count_inputnum = input(&a, &b, &c);
-    }
-
+    BodyOfProgramm();
     return 0;
 }
 
@@ -33,16 +23,17 @@ int input(float *ptra, float *ptrb, float *ptrc)
     return scanf("%f%f%f", ptra, ptrb, ptrc);
 }
 
-float squareroot(float const square) 
+float squareroot(const float square) 
 {
     float low = 0;
     float high = square;
     int variable_int;
+    float accuracy = 0.01;
 
     if (square < 1)
         high = 1;
 
-    while ((high - low) > 0.01)
+    while ((high - low) > accuracy)
     {
         float mid = (low + high) / 2;
         if (mid * mid < square)
@@ -54,28 +45,66 @@ float squareroot(float const square)
     return low;
 }
 
-void findsolution(const float a, const float b, const float c, int * ptrway_of_solution)
+void findsolution(const float a, const float b, const float c, int * ptrMeaningOfad)
 {
     float d = b * b - 4 * a * c;
     if (a == 0 && b == 0 && c == 0)
-        *ptrway_of_solution = 1;
+        *ptrMeaningOfad = a_and_d_equal_to_zero; 
     else if (a == 0 && b != 0 && d != 0)
-        *ptrway_of_solution = 2; 
+        *ptrMeaningOfad = only_a_equal_to_zero; 
     else if (d == 0 && a != 0)
-        *ptrway_of_solution = 3;
+        *ptrMeaningOfad = only_d_equal_to_zero;
     else if (d > 0 && a != 0)
-        *ptrway_of_solution = 4;
+        *ptrMeaningOfad = a_and_d_equal_to_notzero;
 }
 
-void output(const float a, const float b, const float c, const int way_of_solution)
+void output(const float a, const float b, const float c, const int MeaningOfad)
 {
-    float sqrt_d = squareroot(b * b - 4 * a * c);
-    switch(way_of_solution)
+    float d = b * b - 4 * a * c;
+    float sqrt_d = squareroot(d);
+    float x01 = -c / b == 0 ? 0 : -c / b;
+    float x10 = -b / (2 * a) == 0 ? 0 : -b / (2 * a);
+    float x11_1 = (-b + sqrt_d) / (2 * a);
+    float x11_2 = (-b - sqrt_d) / (2 * a);
+
+
+    switch(MeaningOfad)
     {
-        case 1: printf("Тяжело...\n корней бесконечно много\n"); break;
-        case 2: printf("x = %.2f\n", (float)-c / b == 0 ? 0 : -c / b); break;
-        case 3: printf("1 корень: x = %.2f\n", (float)-b / (2 * a) == 0 ? 0 : -b / (2 * a)); break;
-        case 4: printf("2 корня:\n x = %.2f\n x = %.2f\n", (float)(-b + sqrt_d) / (2 * a), (float)(-b - sqrt_d) / (2 * a)); break;
+        case a_and_d_equal_to_zero: printf("Тяжело...\n корней бесконечно много\n"); break;
+        case only_a_equal_to_zero: printf("x = %.2f\n", x01); break;
+        case only_d_equal_to_zero: printf("1 корень: x = %.2f\n", x10); break;
+        case a_and_d_equal_to_notzero: printf("2 корня:\n x = %.2f\n x = %.2f\n", x11_1, x11_2); break;
         default: printf("корней нет\n");
     }
+}
+
+char getanswer()
+{
+    char choice;
+    choice = getchar(); 
+    while (getchar() != '\n') 
+        continue; 
+    return choice;
+}
+void BodyOfProgramm()
+{
+    float a = 0;
+    float b = 0;
+    float c = 0;
+    int count_inputnum = 0;
+    int MeaningOfad = 0;
+
+    count_inputnum = input(&a, &b, &c);
+
+    while (count_inputnum == 3)
+    {
+        findsolution(a, b, c, &MeaningOfad);
+        output(a, b, c, MeaningOfad);
+        count_inputnum = input(&a, &b, &c);
+    }
+    char last_notright_input = getanswer();
+    printf("Вы точно хотите выйти из программы?\n");
+    char answer = getanswer();
+    if (answer == 'n')
+        BodyOfProgramm();
 }
